a87c6a727d4c1aba1825f092467c38458708321d
Aria Dolatabadian <74664712+Aria-Dolatabadian@users.noreply.github.com>
2023-02-24 11:14:44 +0800

    Update Code.R

--- Code.R
+++ Code.R
@@ -54,3 +54,26 @@ p <- heatmaply(mat,
         )
 
 p
+
+
+p <- heatmaply(mat, 
+        #dendrogram = "row",
+        xlab = "", ylab = "", 
+        main = "",
+        scale = "column",
+        margins = c(60,100,40,20),
+        grid_color = "white",
+        grid_width = 0.00001,
+        titleX = FALSE,
+        hide_colorbar = TRUE,
+        branches_lwd = 0.1,
+        label_names = c("Country", "Feature:", "Value"),
+        fontsize_row = 5, fontsize_col = 5,
+        labCol = colnames(mat),
+        labRow = rownames(mat),
+        heatmap_layers = theme(axis.line=element_blank())
+        )
+
+p
+
+
